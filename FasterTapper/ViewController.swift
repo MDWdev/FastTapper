c61ef059de0cbbb12b05acccd61754af1b73d3b0
Melissa Webster <mwebdesign@icloud.com>
2015-09-22 08:53:57 -0400

    Initial Working App from 092115

--- FasterTapper/ViewController.swift
+++ FasterTapper/ViewController.swift
@@ -9,15 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+   
+    
+    var timer = NSTimer()
+    var count = 0
+    var seconds = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupGame()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupGame() {
+        seconds = 60
+        count = 0
+        
+        countdownLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+    }
+    
+    func subtractTime() {
+        seconds--
+        countdownLabel.text = "Time: \(seconds)"
+        
+        if(seconds == 0) {
+            timer.invalidate()
+            let alert = UIAlertController(title: "Time is up!",
+                message: "You scored \(count) points",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Play Again",
+                style :UIAlertActionStyle.Default,
+                handler: { action in self.setupGame()
+            }))
+            presentViewController(alert, animated: true, completion:nil)
+        }
+    }
+    
+    @IBAction func buttonPressed() {
+        count++
+        scoreLabel.text = "Score: \(count)"
     }
 
 
